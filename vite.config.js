5ebb7cfe7b685caf15dbb966696c2ad951507dda
b-gowland <bengowland@gmail.com>
2026-05-22 17:57:34 +1000

    Add sitemap and robots.txt for Google Search Console

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://baseline.airiskpractice.org',
+      dynamicRoutes: [
+        '/configure',
+        '/output',
+        '/example',
+        '/about',
+        '/disclaimer',
+      ],
+      exclude: ['/404'],
+    }),
+  ],
   base: '/',
   build: {
     outDir: 'dist',
